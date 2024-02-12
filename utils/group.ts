3998947bfabf1944ef718250e06e356060500648
Zihan Hu <hu-zihan@foxmail.com>
2024-02-12 15:12:03 +0800

    perf: 使用封装器脚本、动态导入 xlsx 库

--- utils/group.ts
+++ utils/group.ts
@@ -1,4 +1,3 @@
-import xlsx from 'xlsx'
 import type { RollCallOption } from './roll-call'
 
 export const DEFAULT_GROUP_OPTIONS: RollCallOption[] = ['弭儿', '艾达', 'Kouma', '鸠', '伯尼斯', 'Branda', 'Dlyro', '艾尔帕克', 'Fuli', '阿爽', 'Lily', 'Carl', '克洛诺斯', 'Findstr', 'Theo', '久住', '尊师古卢', '萨芙', '帕拉斯', '门图', 'Igallta', 'Gino', 'Sultan']
@@ -46,13 +45,16 @@ export function generateNewGroupName() {
 
 /** 从 Excel 文件中读取名单。 */
 export async function importGroupFromExcel(file: File) {
-  const workbook = xlsx.read(await file.arrayBuffer(), { type: 'array' })
+  const { read, utils } = await import('./xlsx')
+
+  const workbook = read(await file.arrayBuffer(), { type: 'array' })
   const worksheet = workbook.Sheets[workbook.SheetNames[0]]
-  const rows = xlsx.utils.sheet_to_json(worksheet, {
+  const rows = utils.sheet_to_json(worksheet, {
     blankrows: false,
     header: 1,
     skipHidden: true,
   }) as unknown[][]
+
   return (rows // 去除非字符串，去除空元素
     .filter(v => typeof v[0] === 'string') as string[][])
     .map(v => v[0].trim())
---
+++ utils/xlsx.ts
@@ -0,0 +1,5 @@
+// https://docs.sheetjs.com/docs/getting-started/installation/frameworks#dynamic-imports
+
+import { read, utils } from 'xlsx'
+
+export { read, utils }
