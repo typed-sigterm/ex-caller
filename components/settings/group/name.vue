7ede0f7f9431c1ba7b1af70feb60a85e2002be2a
Zihan Hu <hu-zihan@foxmail.com>
2024-02-24 12:30:36 +0800

    fix: 修复重命名名单时自动切换到目标名单的问题

--- components/settings/group/name.vue
+++ components/settings/group/name.vue
@@ -40,7 +40,7 @@ function handleRename() {
           :maxlength="MAX_GROUP_NAME_LENGTH"
         />
         <template #trigger>
-          <NButton class="operator ml-1" size="tiny" text>
+          <NButton class="operator ml-1" size="tiny" text @click.stop>
             <NaiveIcon name="ep:edit" :size="14" />
           </NButton>
         </template>
